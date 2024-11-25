76d327dbc816a81427ce47abf11a9fdca8b20db7
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-26 00:16:37 +0530

    fix errors

--- assignment01/src/App.js
+++ assignment01/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import Arts from './components/Arts';
+import React,{useState} from 'react';
+
 
 function App() {
+  const [theme,setTheme]=useState(true);
+    const dark={
+        backgroundColor:"black",
+        color:"white"
+    }
+    const light={
+        backgroundColor:"white",
+        color:"black"
+    }
   return (
     <div>
+      <div style={theme?dark:light}>
+            <button onClick={()=>setTheme(!theme)}>{theme?'dark':'light'}</button>
+
+        </div>
+      
          <Arts></Arts>
     </div>
   );
